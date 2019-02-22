a8e34b00d7db9d0065940409ebb02ac9eb77862a
sandy1565 <40385371+sandy1565@users.noreply.github.com>
2019-02-22 12:21:57 +0530

    a

--- src/app/advertisement-publish/advertise-publish/advertise-publish.component.ts
+++ src/app/advertisement-publish/advertise-publish/advertise-publish.component.ts
@@ -10,143 +10,37 @@ import { CommonService } from '../../services/commonService';
   styleUrls: ['./advertise-publish.component.scss']
 })
 
-export class AdvertisePublishComponent implements OnInit  {
-  constructor(private advertisepublishService: AdvertisePublishService, private commonService: CommonService) {  }
+export class AdvertisePublishComponent implements OnInit {
+  constructor(private advertisepublishService: AdvertisePublishService, private commonService: CommonService) { }
   userSelectsString = '';
-  locations = [];
-  blocks = [];
-  fromAge;
-  toAge;
   age_as_per_advtPublishDate;
-  selectedBlockId;
-  blockId;
-  selectionLocationId;
-  locationId;
   show = false;
   userSelects = [];
   gender = ['Male', 'Female', 'Transgender'];
   genderModel = new Gender();
-  countries = [];
-  states = [];
-  cities = [];
-  selectedCountry = 1;
-  selectedLocationName;
-  selectedFloorType;
-  selectedStateName;
-  selectedCityName;
-  selectedState;
-  selectedCity;
-  countryList;
-  stateId;
-  cityId;
-  floorId;
   textMessage;
 
   ngOnInit() {
-    this.blocks = [];
-    this.locations = [];
-
-    this.commonService.getCountryDetails().subscribe(countryList => {
-        localStorage.setItem('countryDetails', JSON.stringify(countryList));
-        this.onSelectCountry(this.selectedCountry);
-      });
-
-    this.commonService.getBlock().subscribe(data => {
-      this.blocks = data;
-    });
-
   }
 
-  onSelectCountry(country_id: number) {
-    this.selectedCountry = country_id;
-    this.states = this.getStates().filter(item => {
-      return item.country_id === Number(country_id);
-    });
-  }
-
-  onSelectState(state_id: number) {
-    this.selectedState = state_id;
-    this.selectedStateName = this.selectedState;
-    this.cities = this.getCity().filter(item => {
-      return item.state_id === Number(state_id);
-    });
-  }
-
-  onSelectCity(city_id: number) {
-    this.selectedCity = city_id;
-    const locationData = {
-      country_id: this.selectedCountry,
-      state_id: this.selectedState,
-      city_id: this.selectedCity
-    };
-
-    this.getLocations(locationData);
-    this.locations = this.getLocations(locationData).filter(item => {
-      return item.city_id === Number(city_id);
-    });
-  }
-
-  getLocations(locationData) {
-    const request_data = new URLSearchParams();
-    this.locations = [];
-    request_data.set('locationData', locationData);
-    this.commonService.getLocation(locationData).subscribe(data => {
-      return (this.locations = JSON.parse(
-        (JSON.stringify(data))
-        ));
-    });
-    return this.locations;
-  }
-
-  getCountries() {
-    return JSON.parse(localStorage.getItem('countryDetails'));
-  }
-
-  getStates() {
-    return JSON.parse(localStorage.getItem('stateDetail'));
-  }
-
-  getCity() {
-    return JSON.parse(localStorage.getItem('cityDetails'));
-  }
-
-  selectLocation() {
-    this.selectedLocationName = this.locationId;
-  }
-
-  getValue(e){
-    this.gender = e.target.value;
-  }
   
-  selectLocationName() { 
-      this.selectionLocationId = this.locationId;
-  } 
- 
-  selectBlockName() {
-    this.selectedBlockId = this.blockId;
+  getValue(e) {
+    this.gender = e.target.value;
   }
 
   assignToNgModel() {
     this.userSelectsString = '';
     this.userSelects.map((item) => this.userSelectsString += item.locationName + ' ');
   }
- 
-  publishDetails(event, fromAge, toAge, textMessage) {  
+
+  publishDetails(event, fromAge, toAge, textMessage) {
     const publishModel = {};
     publishModel['gender'] = this.genderModel.gender;
-    publishModel['fromAge'] = fromAge;
-    publishModel['toAge'] = toAge;
-    publishModel['location_id'] = this.userSelects;
-    publishModel['block_id'] = this.selectedBlockId;
-    publishModel['country_id'] = this.selectedCountry;
-    publishModel['state_id'] = this.selectedState;
-    publishModel['city_id'] = this.selectedCity;
-    publishModel['location_id'] = this.selectedLocationName;
     publishModel['text_message'] = textMessage;
-    this.advertisepublishService.addPublish(publishModel).subscribe((data)=> {
-        console.log('======================', publishModel);
-        console.log('Successfully added !');
-      });
-    }
+    this.advertisepublishService.addPublish(publishModel).subscribe((data) => {
+      console.log('======================', publishModel);
+      console.log('Successfully added !');
+    });
   }
+}
 
--- src/app/advertisment-details/advt-details/advt-details.component.ts
+++ src/app/advertisment-details/advt-details/advt-details.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { NgForm } from "@angular/forms";
 import { AdvtDetailsService } from "../services/advt-details.service";
-import { ClientModel } from "../services/advtDetails.model";
-import { HttpHeaders } from "@angular/common/http";
+import { CommonService } from '../../services/commonService';
 
 @Component({
   selector: 'app-advt-details',
@@ -10,9 +8,26 @@ import { HttpHeaders } from "@angular/common/http";
   styleUrls: ['./advt-details.component.scss']
 })
 export class AdvtDetailsComponent implements OnInit {
-  constructor(private advtService: AdvtDetailsService) {}
+  constructor(private advtService: AdvtDetailsService, private commonService: CommonService) { }
 
   clients = [];
+  blocks = [];
+  states = [];
+  cities = [];
+  locations = [];
+  selectedCountry;
+  selectedLocationName;
+  selectedFloorType;
+  selectedStateName;
+  selectedCityName;
+  selectedState;
+  selectedCity;
+  countryList;
+  stateId;
+  cityId;
+  selectionLocationId;
+  locationId;
+  floorId;
   clientId;
   advt_details;
   publish_date;
@@ -22,12 +37,82 @@ export class AdvtDetailsComponent implements OnInit {
   selectedClientName;
   getValue;
   advt_subject;
+  client = [];
 
   ngOnInit() {
-    this.clients = [];
     this.advtService.getClient().subscribe(data => {
       this.clients = data;
     });
+
+    this.commonService.getCountryDetails().subscribe(countryList => {
+      localStorage.setItem('countryDetails', JSON.stringify(countryList));
+      this.onSelectCountry(this.selectedCountry);
+    });
+
+    this.commonService.getBlock().subscribe(data => {
+      this.blocks = data;
+    });
+  }
+
+  onSelectCountry(country_id: number) {
+    this.selectedCountry = country_id;
+    this.states = this.getStates().filter(item => {
+      return item.country_id === Number(country_id);
+    });
+  }
+
+  onSelectState(state_id: number) {
+    this.selectedState = state_id;
+    this.selectedStateName = this.selectedState;
+    this.cities = this.getCity().filter(item => {
+      return item.state_id === Number(state_id);
+    });
+  }
+
+  onSelectCity(city_id: number) {
+    this.selectedCity = city_id;
+    const locationData = {
+      country_id: this.selectedCountry,
+      state_id: this.selectedState,
+      city_id: this.selectedCity
+    };
+
+    this.getLocations(locationData);
+    this.locations = this.getLocations(locationData).filter(item => {
+      return item.city_id === Number(city_id);
+    });
+  }
+
+  getLocations(locationData) {
+    const request_data = new URLSearchParams();
+    this.locations = [];
+    request_data.set('locationData', locationData);
+    this.commonService.getLocation(locationData).subscribe(data => {
+      return (this.locations = JSON.parse(
+        (JSON.stringify(data))
+      ));
+    });
+    return this.locations;
+  }
+
+  getCountries() {
+    return JSON.parse(localStorage.getItem('countryDetails'));
+  }
+
+  getStates() {
+    return JSON.parse(localStorage.getItem('stateDetail'));
+  }
+
+  getCity() {
+    return JSON.parse(localStorage.getItem('cityDetails'));
+  }
+
+  selectLocation() {
+    this.selectedLocationName = this.locationId;
+  }
+
+  selectLocationName() {
+    this.selectionLocationId = this.locationId;
   }
 
   selectClientName() {
@@ -37,7 +122,7 @@ export class AdvtDetailsComponent implements OnInit {
 
   onSubmit(event, AdvtDetails, publishDate, ageFrom, ageTo) {
     const advtDetails = {};
-    advtDetails[ 'client_id'] = this.selectedClientName;
+    advtDetails['client_id'] = this.selectedClientName;
     advtDetails['advt_details'] = AdvtDetails;
     advtDetails['publish_date'] = publishDate;
     advtDetails['age_from'] = ageFrom;
